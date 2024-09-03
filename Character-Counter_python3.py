5feacd272ecd73fa4a0d3537cc7fc2ea6bc827da
Anwar Alhabsi <anwr.alhabsii@gmail.com>
2024-09-04 00:34:41 +0400

    Update Character-Counter_python3.py

--- Character-Counter_python3.py
+++ Character-Counter_python3.py
@@ -1,6 +1,6 @@
 # -*- coding: cp1254 -*-
 
-print("Character Counter Tool")
+print("Zbytomd cdensoc kbo zbkmdsmkv cdensoc pyb CYM kxkvicd mkxnsnkdoc lydr sxnsfsnekvvi kxn sx qbyezc. Dro xohd cdkqo yp dro mkxnsnkdoc gry rkfo droybodsmkv uxygvonqo sc dro zbytomd cdensoc. Drobo cryevn xyd lo wemr boaesbowoxdc dy lo klvo dy ny zbytomd gybu. Pyb ohkwzvo, rkfsxq k mywzedob myxxomdon dy dro sxdobxod wki lo ceppsmsoxd pyb dro mkxnsnkdo. K bocokbmr cdeni dy kccscd CYM yzobkdsxq zbkmdsmoc yb dro nofovyzwoxd yp kx kzzvsmkdsyx drkd mkx lo econ pyb CYM gyevn lo qyyn ohkwzvoc yp cemr zbytomdc. Dro mkxnsnkdo kxkvicd mkx nofovyz k cdkxn-kvyxo kzzvsmkdsyx pbyw dro cmbkdmr yb kx knn-yx dy kx ohscdsxq kzzvsmkdsyx. Sx knnsdsyx, droi mkx gybu sx qbyezc kxn myxdbsledo dy vkbqo zbytomdc. Sx lydr mkcoc, dro kxkvicd mkxnsnkdo gsvv crkbzox rsc/rob cusvvc kxn lo fscslvo gsdrsx dro gybvn yp dro SD psovn. Kxcgob sc CswzvoMkockbMszrob.")
 
 a = input("Input : ")
 def uniq(input):
